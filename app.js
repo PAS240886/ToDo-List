64a8480f2febe4f676595dc702b6e2a60c2568ed
Alexander Popov <popov.a.connect@gmail.com>
2023-08-30 13:14:36 +0300

    LocalStorage added

--- app.js
+++ app.js
@@ -11,9 +11,33 @@ function addTask(){
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
         // adding cross symbol at the end of a row
-        let span = document.creatElement("span");
-        span.innerHTML = "/u00d7";
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
     inputBox.value = "";
+    saveData();
 }
+
+// Class switching by clicking. Checked/deleted.
+listContainer.addEventListener ("click", function(e) {
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle ("checked");
+        saveData();
+    } else if(e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false ); 
+
+// local storage
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+};
+
+function showData(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showData();
